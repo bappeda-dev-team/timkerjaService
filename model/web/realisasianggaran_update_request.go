58f86760c0acdb50474f3f1add6530eb451dab79
RyanB1303 <ryan.brilliant.nirwana@gmail.com>
2025-12-09 23:20:51 +0700

    mybe bener

--- model/web/realisasianggaran_update_request.go
+++ model/web/realisasianggaran_update_request.go
@@ -2,15 +2,17 @@ package web
 
 type RealisasiAnggaranUpdateRequest struct {
 	Id                int    `json:"id" validate:"required"`
+	KodeTim           string `json:"kode_tim" validate:"required"`
 	IdPohon           int    `json:"id_pohon" validate:"required"`
+	IdRencanaKinerja  string `json:"id_rencana_kinerja" validate:"required"`
 	KodeSubkegiatan   string `json:"kode_subkegiatan" validate:"required"`
 	RealisasiAnggaran int    `json:"realisasi_anggaran" validate:"required"`
 	KodeOpd           string `json:"kode_opd" validate:"required"`
 	RencanaAksi       string `json:"rencana_aksi"`
 	FaktorPendorong   string `json:"faktor_pendorong"`
 	FaktorPenghambat  string `json:"faktor_penghambat"`
-	RisikoHukum       string `json:"risko_hukum"`
 	RekomendasiTl     string `json:"rekomendasi_tl"`
+	RisikoHukum       string `json:"risko_hukum"`
 	BuktiDukung       string `json:"bukti_dukung"`
 	Bulan             string `json:"bulan" validate:"required"`
 	Tahun             string `json:"tahun" validate:"required"`
